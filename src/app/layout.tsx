10406478a33ea3268285bc90fbf455130099c995
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-17 02:11:48 +0530

    navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
+  title: "Social Dev",
   description: "Social media app built with Next.js",
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-
+import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
@@ -11,9 +11,51 @@ const Navbar = () => {
         </Link>
       </div>
       {/* CENTER */}
-      <div className=""></div>
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
+        {/* LINKS */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/home.png"
+              alt="Homepage"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/friends.png"
+              alt="Friends"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/stories.png"
+              alt="Stories"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+            <span>Stories</span>
+          </Link>
+        </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="" width={14} height={14} />
+        </div>
+      </div>
       {/* RIGHT */}
-      <div className="">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </div>
