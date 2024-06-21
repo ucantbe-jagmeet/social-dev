1acae5fb8e1efc6939cedc8c3d22ebc6c0b5cad4
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-22 02:41:10 +0530

    homepage updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ const Homepage = () => {
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">{/* left menu */}</div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
-        <div className="flex flex-col gap-6">
-          {/* stories */}
-          {/* Addpost */}
-          {/* Feed */}
-        </div>
+        <div className="flex flex-col gap-6"></div>
       </div>
       <div className="hidden lg:block w-[30%]">{/* Right menu */}</div>
     </div>
