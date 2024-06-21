524c0cb625bd7105501e54708a29e60399e3049d
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-22 02:40:49 +0530

    homepage updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">{/* left menu */}</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          {/* stories */}
+          {/* Addpost */}
+          {/* Feed */}
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">{/* Right menu */}</div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
