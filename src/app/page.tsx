0f3f039ad00fe7fd86a908e01709d6e4b68b58d0
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-25 02:55:09 +0530

    homepage updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+import AddPost from "@/components/AddPost";
+import Stories from "@/components/Stories";
+import Feed from "@/components/feed/Feed";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
+import RightMenu from "@/components/rightMenu/RightMenu";
+
 const Homepage = () => {
   return (
     <div className="flex gap-6 pt-6">
-      <div className="hidden xl:block w-[20%]">{/* left menu */}</div>
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
-          {/* stories */}
-          {/* Addpost */}
-          {/* Feed */}
+          <Stories />
+          <AddPost />
+          <Feed />
         </div>
       </div>
-      <div className="hidden lg:block w-[30%]">{/* Right menu */}</div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
     </div>
   );
 };
