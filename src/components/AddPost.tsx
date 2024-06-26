d443b2b3295d1d3a814b6d98c5226f70830f952f
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-27 03:17:33 +0530

    add Post section completed

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import Image from "next/image";
 
 const AddPost = () => {
-  return <div>Add Post</div>;
+  const handleChange = () => {};
+  return (
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+      <Image
+        src={"/noAvatar.png"}
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      <div className="flex-1">
+        <form className="flex gap-4">
+          <textarea
+            placeholder="What's on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
+            onChange={handleChange}
+          ></textarea>
+          <div className="">
+            <Image
+              src="/emoji.png"
+              alt=""
+              width={20}
+              height={20}
+              className="w-5 h-5 cursor-pointer self-end"
+            />
+          </div>
+        </form>
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => open()}
+          >
+            <Image src="/addimage.png" alt="" width={20} height={20} />
+            Photo
+          </div>
+
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
