b066ed2e1d946ba0b89a4f64e1e36264222dc12f
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-03 03:24:22 +0530

    Post page and post page added

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
+      <Post />
+    </div>
+  );
 };
 
 export default Feed;
---
+++ src/components/feed/Post.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src={"/noAvatar.png"}
+            width={40}
+            height={40}
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">Post 1 user</span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            src={
+              "https://images.pexels.com/photos/20861037/pexels-photo-20861037/free-photo-of-woman-on-pier-on-lake-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            fill
+            className="object-cover rounded-md"
+            alt=""
+          />
+        </div>
+        <p>Description goes here</p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
