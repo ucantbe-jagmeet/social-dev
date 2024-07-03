0ee28bbaabc690c492f92bf38006d5c92f68f1df
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-04 03:32:06 +0530

    friend request page added

---
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+
+const FriendRequestList = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <Image
+          src={"/noAvatar.png"}
+          alt=""
+          width={40}
+          height={40}
+          className="w-10 h-10 rounded-full object-cover"
+        />
+        <span className="font-semibold">New Friend</span>
+      </div>
+      <div className="flex gap-3 justify-end">
+        <form>
+          <button>
+            <Image
+              src="/accept.png"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+          </button>
+        </form>
+        <form>
+          <button>
+            <Image
+              src="/reject.png"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FriendRequestList;
--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import FriendRequestList from "./FriendRequestList";
+import Birthdays from "./Birthdays";
 
-const RightMenu = () => {
-  return <div>RightMenu</div>;
+const RightMenu = ({ userId }: { userId?: string }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <FriendRequestList />
+      <Birthdays />
+    </div>
+  );
 };
 
 export default RightMenu;
