31e7dcbd3eebf577cb2333787d9b52675181d531
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-04 04:19:35 +0530

    Left side profile card completed

--- src/components/leftMenu/LeftMenu.tsx
+++ src/components/leftMenu/LeftMenu.tsx
@@ -59,7 +59,7 @@ const menuData = [
 const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
     <div className="flex flex-col gap-6">
-      {type === "home" && <ProfileCard />}
+      {type !== "home" && <ProfileCard />}
       <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
         {menuData.map((item) => {
           return (
--- src/components/leftMenu/ProfileCard.tsx
+++ src/components/leftMenu/ProfileCard.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+const ProfileCard = async () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src={
+            "https://images.pexels.com/photos/7266271/pexels-photo-7266271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/1040880/pexels-photo-1040880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">Jagmeet Singh</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src="https://images.pexels.com/photos/13812367/pexels-photo-13812367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/13812367/pexels-photo-13812367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/13812367/pexels-photo-13812367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+          </div>
+          <span className="text-xs text-gray-500">2198 Followers</span>
+        </div>
+        <Link href={`/`}>
+          <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+            My Profile
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
