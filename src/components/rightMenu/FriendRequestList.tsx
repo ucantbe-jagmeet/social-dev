aed74ce64d92e79d1673e44f4c2b5220cbaba7d8
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-04 03:40:20 +0530

    css added to friend requests

--- src/components/rightMenu/FriendRequestList.tsx
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -3,42 +3,44 @@ import Image from "next/image";
 
 const FriendRequestList = () => {
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center gap-4">
-        <Image
-          src={"/noAvatar.png"}
-          alt=""
-          width={40}
-          height={40}
-          className="w-10 h-10 rounded-full object-cover"
-        />
-        <span className="font-semibold">New Friend</span>
+    <main className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <span className="font-semibold">New Friend</span>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <form>
+            <button>
+              <Image
+                src="/accept.png"
+                alt=""
+                width={20}
+                height={20}
+                className="cursor-pointer"
+              />
+            </button>
+          </form>
+          <form>
+            <button>
+              <Image
+                src="/reject.png"
+                alt=""
+                width={20}
+                height={20}
+                className="cursor-pointer"
+              />
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="flex gap-3 justify-end">
-        <form>
-          <button>
-            <Image
-              src="/accept.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
-          </button>
-        </form>
-        <form>
-          <button>
-            <Image
-              src="/reject.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
-          </button>
-        </form>
-      </div>
-    </div>
+    </main>
   );
 };
 
