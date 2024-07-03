d282089ddd05275b6d3c5ac4c70e538063960e5e
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-04 03:43:12 +0530

    friend list improved

---
+++ src/components/rightMenu/FriendRequests.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import FriendRequestList from "./FriendRequestList";
+
+const FriendRequests = async () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* TOP */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Friend Requests</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+      <FriendRequestList />
+    </div>
+  );
+};
+
+export default FriendRequests;
